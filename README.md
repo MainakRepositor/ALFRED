20dc2a2f8c2a984550978793b975b1b075d931ab
MAINAK CHAUDHURI <sultankhilji001@gmail.com>
2023-03-11 11:52:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FED
+# Real-Time-Face-Detection
+Real Time Face and Emotion Detector using  Face api js
+
+## <a href='https://justadudewhohacks.github.io/face-api.js/docs/index.html'>Face api js</a>
+JavaScript API for face detection and face recognition in the browser implemented on top of the tensorflow.js core API (tensorflow/tfjs-core)
---
+++ script.js
@@ -0,0 +1,37 @@
+// capturing html area
+const video = document.getElementById('video')
+
+//loading all models
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+
+// start video when model is ready
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+// view face recognisation to user
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
